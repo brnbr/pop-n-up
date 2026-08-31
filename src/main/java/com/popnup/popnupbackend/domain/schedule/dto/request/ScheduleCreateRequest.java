628793800a7dce7f487119717ca9c43fe553d269
Inn <dulsfnj413@naver.com>
2026-08-31 22:31:40 +0900

    chore: 변수 이름 오타 수정

--- src/main/java/com/popnup/popnupbackend/domain/schedule/dto/request/ScheduleCreateRequest.java
+++ src/main/java/com/popnup/popnupbackend/domain/schedule/dto/request/ScheduleCreateRequest.java
@@ -1,16 +1,15 @@
 package com.popnup.popnupbackend.domain.schedule.dto.request;
 
-import lombok.Getter;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
+import lombok.Getter;
 
 @Getter
 public class ScheduleCreateRequest {
 
-    private Long popupId;
-    private LocalDate scheduleDate;
-    private LocalTime startTime;
-    private LocalTime endTime;
-    private Integer maxCapaticy;
+  private Long popupId;
+  private LocalDate scheduleDate;
+  private LocalTime startTime;
+  private LocalTime endTime;
+  private Integer maxCapacicy;
 }
