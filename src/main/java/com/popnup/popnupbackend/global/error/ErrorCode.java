73a097e5e0148e9388471c4c51128d85f273fd78
Inn <dulsfnj413@naver.com>
2026-09-02 06:27:26 +0900

    feat(global): add abstract methods to ErrorCode interface

--- src/main/java/com/popnup/popnupbackend/global/error/ErrorCode.java
+++ src/main/java/com/popnup/popnupbackend/global/error/ErrorCode.java
@@ -1,5 +1,9 @@
 package com.popnup.popnupbackend.global.error;
 
+import org.springframework.http.HttpStatus;
+
 public interface ErrorCode {
     String name();
+    HttpStatus getHttpStatus();
+    String getMessage();
 }
