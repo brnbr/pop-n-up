945d6c79b0d0ccc9483754b065fb09858174374c
Inn <dulsfnj413@naver.com>
2026-09-03 19:51:13 +0900

    feat(reservation): implement CRUD operations

--- src/main/java/com/popnup/popnupbackend/domain/reservation/controller/ReservationController.java
+++ src/main/java/com/popnup/popnupbackend/domain/reservation/controller/ReservationController.java
@@ -1,12 +1,54 @@
 package com.popnup.popnupbackend.domain.reservation.controller;
 
+import com.popnup.popnupbackend.domain.auth.dto.request.AuthUser;
+import com.popnup.popnupbackend.domain.reservation.dto.request.ReservationCreateRequest;
+import com.popnup.popnupbackend.domain.reservation.dto.response.ReservationCreateResponse;
+import com.popnup.popnupbackend.domain.reservation.dto.response.ReservationResponse;
 import com.popnup.popnupbackend.domain.reservation.service.ReservationService;
+import com.popnup.popnupbackend.global.common.ApiResponse;
+import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class ReservationController {
 
-    private final ReservationService reservationService;
+  private final ReservationService reservationService;
+
+  // 예약 생성
+  @PostMapping("/reservations")
+  public ResponseEntity<ApiResponse<ReservationCreateResponse>> createResrvation(
+      @AuthenticationPrincipal AuthUser authUser,
+      @Valid @RequestBody ReservationCreateRequest request) {
+    return ResponseEntity.ok(
+        ApiResponse.success(reservationService.book(authUser.getId(), request)));
+  }
+
+  // 예약 취소
+  @DeleteMapping("/reservations/{reservationId}")
+  public ResponseEntity<ApiResponse<Void>> deleteReservation(
+      @AuthenticationPrincipal AuthUser authUser, @PathVariable Long reservationId) {
+    reservationService.cancel(authUser.getId(), reservationId);
+    return ResponseEntity.ok(ApiResponse.success());
+  }
+
+  // 전체 조회
+  @GetMapping("/reservations")
+  public ResponseEntity<ApiResponse<List<ReservationResponse>>> getAll(
+      @AuthenticationPrincipal AuthUser authUser) {
+    List<ReservationResponse> response = reservationService.allReservations(authUser.getId());
+    return ResponseEntity.ok(ApiResponse.success(response));
+  }
+
+  // 단 건 조회
+  @GetMapping("/reservations/{reservationId}")
+  public ResponseEntity<ApiResponse<ReservationResponse>> getOne(
+      @AuthenticationPrincipal AuthUser authUser, @Valid @PathVariable Long reservationId) {
+    return ResponseEntity.ok(
+        ApiResponse.success(reservationService.oneReservation(authUser.getId(), reservationId)));
+  }
 }
