9ab1a62ed669d899d685544221f21b6de9372a39
Inn <dulsfnj413@naver.com>
2026-09-03 19:51:34 +0900

    feat(reservation): create ReservationRepository

--- src/main/java/com/popnup/popnupbackend/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/popnup/popnupbackend/domain/reservation/repository/ReservationRepository.java
@@ -1,7 +1,10 @@
 package com.popnup.popnupbackend.domain.reservation.repository;
 
 import com.popnup.popnupbackend.domain.reservation.entity.Reservation;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+public interface ReservationRepository
+    extends JpaRepository<Reservation, Long>, ReservationRepositoryCustom {
+  Optional<Reservation> findByIdAndMemberId(Long memberId, Long reservationId);
 }
