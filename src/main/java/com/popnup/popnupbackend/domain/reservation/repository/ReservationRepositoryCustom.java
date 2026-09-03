7aac4e7570c8b3962da9bc0bbe0fa1ab1c502601
Inn <dulsfnj413@naver.com>
2026-09-03 19:56:12 +0900

    feat(reservation): add Querydsl custom repository interface

---
+++ src/main/java/com/popnup/popnupbackend/domain/reservation/repository/ReservationRepositoryCustom.java
@@ -0,0 +1,8 @@
+package com.popnup.popnupbackend.domain.reservation.repository;
+
+import com.popnup.popnupbackend.domain.reservation.entity.Reservation;
+import java.util.List;
+
+public interface ReservationRepositoryCustom {
+  List<Reservation> getAllReservation(Long memberId);
+}
