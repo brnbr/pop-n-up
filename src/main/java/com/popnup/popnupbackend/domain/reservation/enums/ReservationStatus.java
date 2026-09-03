c045766a68525d8e9b2711bb3857102a50b39687
Inn <dulsfnj413@naver.com>
2026-09-03 19:52:42 +0900

    feat(reservation): add USED status

--- src/main/java/com/popnup/popnupbackend/domain/reservation/enums/ReservationStatus.java
+++ src/main/java/com/popnup/popnupbackend/domain/reservation/enums/ReservationStatus.java
@@ -6,9 +6,10 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ReservationStatus {
-    PENDING("예약 대기"),
-    CONFIRMED("예약 확정"),
-    CANCELED("예약 취소");
+  PENDING("예약 대기"),
+  CONFIRMED("예약 확정"),
+  CANCELED("예약 취소"),
+  USED("사용됨");
 
-    private final String description;
+  private final String description;
 }
