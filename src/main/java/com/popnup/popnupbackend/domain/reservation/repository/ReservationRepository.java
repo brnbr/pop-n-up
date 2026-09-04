6889a21c4d63feb5e5ffd31defa258db456e9c59
Inn <dulsfnj413@naver.com>
2026-09-04 15:20:00 +0900

    refactor(reservation): 커스텀 메서드로 바꾸려고 코드 옮김

--- src/main/java/com/popnup/popnupbackend/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/popnup/popnupbackend/domain/reservation/repository/ReservationRepository.java
@@ -4,13 +4,10 @@ import com.popnup.popnupbackend.domain.reservation.entity.Reservation;
 import com.popnup.popnupbackend.domain.reservation.enums.ReservationStatus;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository
     extends JpaRepository<Reservation, Long>, ReservationRepositoryCustom {
-  Optional<Reservation> findByIdAndMemberId(Long reservationId, Long memberId);
-
   List<Reservation> findByStatusAndCreatedAtBefore(
       ReservationStatus status, LocalDateTime deadLine);
 }
