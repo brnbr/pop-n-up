4264807ad3044b21713644613981daf454c84ecb
Inn <dulsfnj413@naver.com>
2026-09-04 14:39:45 +0900

    feat(reservation): 미결제 타임아웃 용 커스텀 메서드 추가

--- src/main/java/com/popnup/popnupbackend/domain/reservation/repository/ReservationRepository.java
+++ src/main/java/com/popnup/popnupbackend/domain/reservation/repository/ReservationRepository.java
@@ -1,10 +1,16 @@
 package com.popnup.popnupbackend.domain.reservation.repository;
 
 import com.popnup.popnupbackend.domain.reservation.entity.Reservation;
+import com.popnup.popnupbackend.domain.reservation.enums.ReservationStatus;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository
     extends JpaRepository<Reservation, Long>, ReservationRepositoryCustom {
   Optional<Reservation> findByIdAndMemberId(Long reservationId, Long memberId);
+
+  List<Reservation> findByStatusAndCreatedAtBefore(
+      ReservationStatus status, LocalDateTime deadLine);
 }
