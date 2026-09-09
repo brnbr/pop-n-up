6ea73ac1790142575b943fa9a1610cb20b2a33b4
Inn <dulsfnj413@naver.com>
2026-09-10 01:54:57 +0900

    refactor: 타임아웃 처리 메서드 수정

--- src/main/java/com/popnup/popnupbackend/domain/reservation/scheduler/ReservationScheduler.java
+++ src/main/java/com/popnup/popnupbackend/domain/reservation/scheduler/ReservationScheduler.java
@@ -1,6 +1,7 @@
 package com.popnup.popnupbackend.domain.reservation.scheduler;
 
 import com.popnup.popnupbackend.domain.reservation.service.ReservationService;
+import com.popnup.popnupbackend.domain.reservation.service.ReservationTimeoutProcessor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -12,13 +13,14 @@ import org.springframework.stereotype.Component;
 public class ReservationScheduler {
 
   private final ReservationService reservationService;
+  private final ReservationTimeoutProcessor reservationTimeoutProcessor;
 
   // 미결제 타임아웃 예약 자동 취소 (매 분 실행)
   @Scheduled(cron = "0 * * * * *")
-  public void cancelExpriedPendingReservations() {
+  public void cancelExpiredPendingReservations() {
     log.debug("[Scheduler] 미결제 예약 만료 처리 스케줄러 시작");
     try {
-      reservationService.payTimeOut();
+      reservationTimeoutProcessor.payTimeOut();
       log.debug("[Scheduler] 미결제 예약 만료 처리 스케줄러 종료");
     } catch (Exception e) {
       log.error("[Scheduler] 미결제 예약 만료 처리 중 예외 발생", e);
