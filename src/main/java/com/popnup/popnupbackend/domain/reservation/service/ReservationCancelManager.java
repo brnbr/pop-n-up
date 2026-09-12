265b8b5384a43f2d5c5994edad1bde9245e724a7
Inn <dulsfnj413@naver.com>
2026-09-12 21:43:17 +0900

    refactor: 락 상태에서 조회/검증/변경하도록 메서드 수정

--- src/main/java/com/popnup/popnupbackend/domain/reservation/service/ReservationCancelManager.java
+++ src/main/java/com/popnup/popnupbackend/domain/reservation/service/ReservationCancelManager.java
@@ -2,6 +2,8 @@ package com.popnup.popnupbackend.domain.reservation.service;
 
 import com.popnup.popnupbackend.domain.reservation.entity.Reservation;
 import com.popnup.popnupbackend.domain.reservation.enums.ReservationStatus;
+import com.popnup.popnupbackend.domain.reservation.exception.ReservationErrorCode;
+import com.popnup.popnupbackend.domain.reservation.repository.ReservationRepository;
 import com.popnup.popnupbackend.domain.schedule.entity.Schedule;
 import com.popnup.popnupbackend.domain.schedule.exception.ScheduleErrorCode;
 import com.popnup.popnupbackend.domain.schedule.repository.ScheduleRepository;
@@ -14,19 +16,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class ReservationCancelManager {
 
   private final ScheduleRepository scheduleRepository;
+  private final ReservationRepository reservationRepository;
 
-  // 스케줄 락 획득 -> 상태 변경 -> 인원 복구
   @Transactional
-  public void cancel(Reservation reservation) {
+  public void cancel(Long reservationId) {
+    Reservation reservation = reservationRepository.findByIdWithPessimisticLock(reservationId)
+            .orElseThrow(ReservationErrorCode.RESERVATION_NOT_FOUND::toException);
+
     if (reservation.getStatus() == ReservationStatus.CANCELED) {
       return;
     }
 
     Long scheduleId = reservation.getSchedule().getId();
-    Schedule schedule =
-        scheduleRepository
-            .findByIdWithPessimisticLock(scheduleId)
-            .orElseThrow(ScheduleErrorCode.SCHEDULE_NOT_FOUND::toException);
+    Schedule schedule = scheduleRepository.findByIdWithPessimisticLock(scheduleId).orElseThrow(ScheduleErrorCode.SCHEDULE_NOT_FOUND::toException);
 
     reservation.cancel();
     schedule.cancelReservation(reservation.getPersonCount());
