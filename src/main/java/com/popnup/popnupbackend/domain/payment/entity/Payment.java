f5c6a5914485beb2ff8e78b42c052aed5823ca1d
jaong <jioh035@kakao.com>
2026-09-14 23:35:12 +0900

    ci를 위한 commit

--- src/main/java/com/popnup/popnupbackend/domain/payment/entity/Payment.java
+++ src/main/java/com/popnup/popnupbackend/domain/payment/entity/Payment.java
@@ -44,7 +44,7 @@ public class Payment {
     this.tid = tid;
   }
 
-  // 결제 상태를 하나의 상태 머신처럼 보기
+  // 결제 상태를 하나의 상태 머신처럼 보기 .
   public void approve() {
     if (this.status != PaymentStatus.READY) {
       throw new IllegalStateException("결제 승인 가능한 상태가 아닙니다.");
