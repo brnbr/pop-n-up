7046dfef94208c6805b1e1d67fb96d76e6b809dc
Inn <dulsfnj413@naver.com>
2026-09-15 19:46:34 +0900

    chore: 공백 수정

--- src/main/java/com/popnup/popnupbackend/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/popnup/popnupbackend/domain/schedule/repository/ScheduleRepository.java
@@ -3,5 +3,4 @@ package com.popnup.popnupbackend.domain.schedule.repository;
 import com.popnup.popnupbackend.domain.schedule.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ScheduleRepository
-    extends JpaRepository<Schedule, Long>, ScheduleRepositoryCustom {}
+public interface ScheduleRepository extends JpaRepository<Schedule, Long>, ScheduleRepositoryCustom {}
