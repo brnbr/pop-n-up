85a56a2a6bcba096390463c713fcc16f36510360
Inn <dulsfnj413@naver.com>
2026-09-15 22:25:58 +0900

    style: 포맷적용

--- src/main/java/com/popnup/popnupbackend/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/popnup/popnupbackend/domain/schedule/repository/ScheduleRepository.java
@@ -3,4 +3,5 @@ package com.popnup.popnupbackend.domain.schedule.repository;
 import com.popnup.popnupbackend.domain.schedule.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ScheduleRepository extends JpaRepository<Schedule, Long>, ScheduleRepositoryCustom {}
+public interface ScheduleRepository
+    extends JpaRepository<Schedule, Long>, ScheduleRepositoryCustom {}
